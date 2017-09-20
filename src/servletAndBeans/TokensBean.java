22612ce7719c96ecba2fd9de589e85412d51d3f2
Jake Erickson <jerickson@mymail.mines.edu>
2017-09-19 20:07:11 -0600

    Added tokenBean

---
+++ src/servletAndBeans/TokensBean.java
@@ -0,0 +1,12 @@
+package servletAndBeans;
+
+import java.io.Serializable;
+
+public class TokensBean implements Serializable {
+	
+	public TokensBean(){
+		
+	}
+	
+	//private Map<String,Friends> newUsersMap = new HashMap<String,Friends>();
+}
